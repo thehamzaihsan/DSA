326136ed6886d1477bf42cd75a497319aed19515
Hamza Ihsan <thehamzaihsan@gmail.com>
2024-05-16 22:38:06 +0500

    lab 10

--- [99] Headers/BST.h
+++ [99] Headers/BST.h
@@ -8,8 +8,77 @@ public:
     BinaryTreeNode *root = nullptr;
     void Insert(int data);
     void Delete();
+    BinarySearchTree()
+    {
+        root = nullptr;
+    }
     BinaryTreeNode *Search(BinaryTreeNode *root, int key);
     void Traverse(BinaryTreeNode *root);
+    void InsertRec(int data, BinaryTreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            BinaryTreeNode *newNode = new BinaryTreeNode;
+            newNode->val = data;
+            newNode->left = nullptr;
+            newNode->right = nullptr;
+            root = newNode;
+        }
+        else
+        {
+            if (data < root->val)
+            {
+                InsertRec(data, root->left);
+            }
+            else
+            {
+                InsertRec(data, root->right);
+            }
+        }
+    }
+    BinaryTreeNode *SearchIter(int key)
+    {
+        BinaryTreeNode *temp = root;
+        while (temp != nullptr)
+        {
+            if (temp->val == key)
+            {
+                return temp;
+            }
+            else if (temp->val > key)
+            {
+                temp = temp->left;
+            }
+            else
+            {
+                temp = temp->right;
+            }
+        }
+    }
+    ~BinarySearchTree()
+    {
+        int count = 0;
+        BinaryTreeNode *left = root->left;
+        BinaryTreeNode *right = root->right;
+        delete root;
+        count++;
+        while (left != nullptr)
+        {
+            BinaryTreeNode *temp = left;
+            left = left->left;
+            delete temp;
+            count++;
+        }
+        while (right != nullptr)
+        {
+            BinaryTreeNode *temp = right;
+            right = right->right;
+            delete temp;
+            count++;
+        }
+
+        // cout << "deleted: " << count << endl;
+    }
     bool isEmpty()
     {
         if (root == nullptr)
@@ -70,7 +139,7 @@ public:
         {
             return 0;
         }
-        return 1 + countNonLeafNodesRecursive(root->left) +  countNonLeafNodesRecursive(root->right);
+        return 1 + countNonLeafNodesRecursive(root->left) + countNonLeafNodesRecursive(root->right);
     }
     int sizeRecursive(BinaryTreeNode *root)
     {
@@ -79,6 +148,105 @@ public:
             return 0;
         }
         return 1 + sizeRecursive(root->left) + sizeRecursive(root->right);
+    }
+    BinaryTreeNode *Max(BinaryTreeNode *temp)
+    {
+        if (temp->right == nullptr)
+        {
+            return temp;
+        }
+        else
+        {
+
+            return Max(temp->right);
+        }
+    }
+    BinaryTreeNode *Min(BinaryTreeNode *temp)
+    {
+        if (temp->left == nullptr)
+        {
+            return temp;
+        }
+        else
+        {
+            return Min(temp->left);
+        }
+    }
+
+    void Delete(int key)
+    {
+        BinaryTreeNode *temp = root;
+        BinaryTreeNode *DeleteNode = Search(root, key);
+        cout << "DeleteNode: " << DeleteNode->val << endl;
+        if (DeleteNode->left == nullptr && DeleteNode->right == nullptr)
+        {
+            delete DeleteNode;
+        }
+        else if (DeleteNode->left == nullptr)
+        {
+            BinaryTreeNode *tempN = DeleteNode;
+            DeleteNode = DeleteNode->right;
+            delete tempN;
+        }
+        else if (DeleteNode->right == nullptr)
+        {
+            BinaryTreeNode *tempN = DeleteNode;
+            DeleteNode = DeleteNode->left;
+            delete tempN;
+        }
+        else if (DeleteNode->left != nullptr && DeleteNode->right != nullptr)
+        {
+            BinaryTreeNode *tempN = Min(DeleteNode->right);
+            // cout << "tempN: " << tempN->val << endl;
+            DeleteNode->val = tempN->val;
+            tempN->val = 0;
+            delete tempN;
+        }
+    }
+    int height(BinaryTreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+        int left = height(root->left);
+        int right = height(root->right);
+        if (left > right)
+        {
+            return left + 1;
+        }
+        else
+        {
+            return right + 1;
+        }
+    }
+
+    void levelOrder(BinaryTreeNode *root)
+    {
+
+        int height_tree = height(root);
+
+        for (int i = 0; i < height_tree; i++)
+        {
+            print_height(root, i);
+        }
+    }
+
+    void print_height(BinaryTreeNode *root, int level)
+    {
+        if (!root)
+        {
+            return;
+        }
+        else
+        {
+            if (level == 0)
+            {
+                cout << root->val << " ";
+            }
+        }
+        print_height(root->left, level - 1);
+        print_height(root->right, level - 1);
     }
 };
 
@@ -97,6 +265,12 @@ void BinarySearchTree::Insert(int data)
         BinaryTreeNode *temp = root;
         while (temp != nullptr)
         {
+            if (newNode->val == temp->val)
+            {
+                cout << "Duplicate value\n";
+                delete newNode;
+                break;
+            }
             if (data < temp->val)
             {
                 if (temp->left == nullptr)
--- [99] Headers/tester.cpp
+++ [99] Headers/tester.cpp
@@ -6,38 +6,78 @@ using namespace std;
 
 int main()
 {
-   
-   BinarySearchTree tree;
-   int choice;
-   do
-   {
-      cout << "1. Insert\n2. Count Leaf Nodes\n3. Count Nodes\n4. Count Non-Leaf Nodes\n5. Exit\n";
-      cout << "Enter your choice: ";
-      cin >> choice;
-      switch (choice)
-      {
-      case 1:
-         int data;
-         cout << "Enter data: ";
-         cin >> data;
-         tree.Insert(data);
-         break;
-      case 2:
-         cout << "Leaf Nodes: " << tree.countLeafNodesRecursive(tree.root) << endl;
-         break;
-      case 3:
-         cout << "Nodes: " << tree.sizeRecursive(tree.root) << endl;
-         break;
-      case 4:
-         cout << "Non-Leaf Nodes: " << tree.countNonLeafNodesRecursive(tree.root) << endl;
-         break;
-      case 5:
-         cout << "Exiting...";
-         break;
-      default:
-         cout << "Invalid choice\n";
-         break;
-      }
-   } while (choice != 5);
-  
+   // BinarySearchTree tree;
+   // int choice;
+   // do
+   // {
+   //    cout << "1. Insert\n2. Count Leaf Nodes\n3. Count Nodes\n4. Count Non-Leaf Nodes\n5. Find Maximum\n6. Find Minimum\n7. Exit\n";
+   //    cout << "Enter your choice: ";
+   //    cin >> choice;
+   //    switch (choice)
+   //    {
+   //    case 1:
+   //       int data;
+   //       cout << "Enter data: ";
+   //       cin >> data;
+   //       tree.Insert(data);
+   //       break;
+   //    case 2:
+   //       cout << "Leaf Nodes: " << tree.countLeafNodesRecursive(tree.root) << endl;
+   //       break;
+   //    case 3:
+   //       cout << "Nodes: " << tree.sizeRecursive(tree.root) << endl;
+   //       break;
+   //    case 4:
+   //       cout << "Non-Leaf Nodes: " << tree.countNonLeafNodesRecursive(tree.root) << endl;
+   //       break;
+   //    case 5:
+   //       if (tree.root == nullptr)
+   //       {
+   //          cout << "Tree is empty\n";
+   //       }
+   //       else
+   //       {
+   //          cout << "Maximum: " << tree.Max(tree.root->right)->val << endl;
+   //       }
+   //       break;
+   //    case 6:
+   //       if (tree.root == nullptr)
+   //       {
+   //          cout << "Tree is empty\n";
+   //       }
+   //       else
+   //       {
+   //          // cout << "Minimum: " << tree.Min(tree.root)->data << endl;
+   //       }
+   //       break;
+   //    case 7:
+   //       cout << "Exiting...";
+   //       break;
+   //    default:
+   //       cout << "Invalid choice\n";
+   //       break;
+   //    }
+   // } while (choice != 7);
+   BinarySearchTree tree; 
+   tree.Insert(10);
+   tree.Insert(5);
+   tree.Insert(15);
+   tree.Insert(3);
+   tree.Insert(7);
+   tree.Insert(12);
+   tree.Insert(18);
+   tree.Insert(1);
+   tree.Insert(4);
+   tree.Insert(6);
+   tree.Insert(8);
+   tree.Insert(11);
+   tree.Insert(13);
+
+   tree.InOrderTraverse(tree.root);
+   cout << endl;
+   tree.PostOrderTraverse(tree.root);
+   cout << endl;
+   tree.PreOrderTraverse(tree.root);
+   cout << endl;
+   // tree.InOrderTraverse(tree.root);
 }
